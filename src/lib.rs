8e796732593afb37ea0153371eaaf2d188f8bbf0
Diego Duarte <diego.duarte@geniussports.com>
2025-01-08 16:46:29 -0500

    Enhance Cache implementation with miss handler, entry expiration management. Added retrieve or compute function

--- src/lib.rs
+++ src/lib.rs
@@ -3,16 +3,29 @@ use std::hash::Hash;
 use std::num::NonZeroUsize;
 use std::time::{Duration, Instant};
 
+#[derive(Debug)]
 struct Entry<D> {
     data: D,
+    adhoc_code: u8,
     expiration: Instant,
 }
 
-impl<D> Entry<D> {
-    fn new(data: D, expiration: Instant) -> Self {
+impl<D: Default> Entry<D> {
+
+    fn default() -> Self {
+        Entry {
+            data: Default::default(),
+            expiration: Instant::now(),
+            adhoc_code: 0,
+        }
+    }
+
+
+    fn new(data: D, expiration: Instant, adhoc_code: u8) -> Self {
         Entry {
             data,
             expiration,
+            adhoc_code,
         }
     }
 
@@ -21,15 +34,21 @@ impl<D> Entry<D> {
     }
 }
 
-pub struct Cache<K: Eq + Hash, D> {
+type MissHandler<K, D> = fn(&K, &mut D, &mut u8) -> bool;
+
+pub struct Cache<K, D> {
     lru_cache: LruCache<K, Entry<D>>,
+    miss_handler: MissHandler<K, D>,
     positive_ttl: Duration, // seconds
+    negative_ttl: Duration, // seconds
 }
 
-impl<K: Eq + Hash, D: Eq> Cache<K, D> {
+impl<K: Eq + Hash + Copy, D: Eq + Default + Copy> Cache<K, D> {
     pub fn new(
         size: usize,
-        ttl: Duration,
+        miss_handler: MissHandler<K, D>,
+        positive_ttl: Duration,
+        negative_ttl: Duration,
     ) -> Self {
         let hash_builder = DefaultHasher::default();
         Cache {
@@ -37,36 +56,62 @@ impl<K: Eq + Hash, D: Eq> Cache<K, D> {
                 NonZeroUsize::new(size).unwrap(),
                 hash_builder,
             ),
-            positive_ttl: ttl,
+            miss_handler,
+            positive_ttl,
+            negative_ttl,
         }
     }
 
-    pub fn insert(&mut self, key: K, data: D) {
+    pub fn insert(&mut self, key: &K, data: &D) {
         let expiration = Instant::now() + self.positive_ttl;
-        let data = Entry::new(data, expiration);
-        self.lru_cache.put(key, data);
+        let entry = Entry::new(*data, expiration, 0);
+        self.lru_cache.put(*key, entry);        
     }
 
     pub fn get(&mut self, key: &K) -> Option<&D> {
-        // Check if the entry exists and is valid
-        let is_valid = if let Some(entry) = self.lru_cache.peek(key) {
-            entry.is_valid()
-        } else {
-            false
-        };
+        self.get_entry(key).map(|entry| &entry.data)
+    }
 
-        // If the entry is valid, return it
-        if is_valid {
-            self.lru_cache.get(key).map(|entry| &entry.data)
-        } else {
-            // If the entry is expired, remove it
-            self.lru_cache.pop(key);
-            None
+    fn get_entry(&mut self, key: &K) -> Option<&Entry<D>> {
+        // First, check if the entry exists and is valid
+        if let Some(entry) = self.lru_cache.get(key) {
+            if entry.is_valid() {
+                return self.lru_cache.get(key);
+            }
         }
+        // If the entry is expired, remove it
+        self.lru_cache.pop(key);
+        None
     }
 
     pub fn len(&self) -> usize {
         self.lru_cache.len()
+    }
+
+    pub fn retrieve_or_compute(&mut self, key: &K) -> (&D, u8) {
+        let miss_handler = self.miss_handler;
+        let positive_ttl = self.positive_ttl;
+        let negative_ttl = self.negative_ttl;
+        
+        if let Some(_) = self.get_entry(key) {
+            // Hit
+            let cache_entry = self.lru_cache.peek(&key).unwrap();
+            return (&cache_entry.data, cache_entry.adhoc_code);
+        }                
+    
+        // Step 2: Handle cache miss
+        let mut entry: Entry<D> = Entry::default();
+
+        if miss_handler(&key, &mut entry.data, &mut entry.adhoc_code) {
+            entry.expiration = Instant::now() + positive_ttl;
+        } else {
+            entry.expiration = Instant::now() + negative_ttl;
+        }
+    
+        // Step 3: Insert the new entry
+        let cache_entry = self.lru_cache.get_or_insert_mut(*key, || entry);
+        (&cache_entry.data, cache_entry.adhoc_code)
+
     }
 }
 
@@ -80,64 +125,74 @@ mod tests {
     use rstest::*;
 
     #[fixture]
-    fn basic() -> Cache<i32, i32> {
+    fn simple_cache() -> Cache<i32, i32> {
+        fn miss_handler(key: &i32, data: &mut i32, adhoc_code: &mut u8) -> bool {
+            if *key == -1 {
+                return false
+            }
+            *data = 2;
+            *adhoc_code+=1;
+            true
+        }
         Cache::new(
             3,
-            Duration::from_secs(1),
+            miss_handler,
+            Duration::from_millis(200),          
+            Duration::from_millis(200),          
         )
     }
 
     #[rstest]
-    fn insert_value(mut basic: Cache<i32, i32>) {
+    fn insert_value(mut simple_cache: Cache<i32, i32>) {
         // Arrange
         let key = 1;
         let value = 2;
 
         // Act
-        basic.insert(key, value);
+        simple_cache.insert(&key, &value);
 
         // Assert
-        assert_eq!(basic.lru_cache.len(), 1);
+        assert_eq!(simple_cache.lru_cache.len(), 1);
     }
 
     #[rstest]
-    fn insert_same_key(mut basic: Cache<i32, i32>) {
+    fn insert_same_key(mut simple_cache: Cache<i32, i32>) {
         // Arrange
         let key = 1;
         let value = 2;
 
         // Act
-        basic.insert(key, value);
-        basic.insert(key, value);
+        simple_cache.insert(&key, &value);
+        simple_cache.insert(&key, &value);
 
         // Assert
-        assert_eq!(basic.lru_cache.len(), 1);
+        assert_eq!(simple_cache.lru_cache.len(), 1);
     }
 
     #[rstest]
-    fn get_value(mut basic: Cache<i32, i32>) {
+    fn get_value(mut simple_cache: Cache<i32, i32>) {
         // Arrange
         let key = 1;
         let value = 2;
 
         // Act
-        basic.insert(key, value);
+        simple_cache.insert(&key, &value);
 
         // Assert
-        assert_eq!(basic.get(&key), Some(&value));
+        assert_eq!(simple_cache.get(&key), Some(&value));
     }
 
     #[rstest]
-    fn get_value_not_found(mut basic: Cache<i32, i32>) {
+    fn get_value_not_found(mut simple_cache: Cache<i32, i32>) {
         // Arrange
         let key = 1;
 
         // Assert
-        assert_eq!(basic.get(&key), None);
+        assert_eq!(simple_cache.get(&key), None);
     }
 
     #[rstest]
-    fn insert_max_capacity(mut basic: Cache<i32, i32>) {
+    fn insert_max_capacity(mut simple_cache: Cache<i32, i32>) {
         // Arrange
         let key1 = 1;
         let key2 = 2;
@@ -146,18 +201,18 @@ mod tests {
         let value = 2;
 
         // Act
-        basic.insert(key1, value);
-        basic.insert(key2, value);
-        basic.insert(key3, value);
-        basic.insert(key4, value);
+        simple_cache.insert(&key1, &value);
+        simple_cache.insert(&key2, &value);
+        simple_cache.insert(&key3, &value);
+        simple_cache.insert(&key4, &value);
 
         // Assert
-        assert_eq!(basic.lru_cache.len(), 3);
-        assert_eq!(basic.get(&key1), None); // lru is removed
+        assert_eq!(simple_cache.lru_cache.len(), 3);
+        assert_eq!(simple_cache.get(&key1), None); // lru is removed
     }
 
     #[rstest]
-    fn get_lru_change(mut basic: Cache<i32, i32>) {
+    fn get_lru_change(mut simple_cache: Cache<i32, i32>) {
         // Arrange
         let key1 = 1;
         let key2 = 2;
@@ -166,37 +221,77 @@ mod tests {
         let value = 2;
 
         // Act
-        basic.insert(key1, value);
-        basic.insert(key2, value);
-        basic.get(&key1); // key2 is now the lru
-        basic.insert(key3, value);
-        basic.insert(key4, value);
+        simple_cache.insert(&key1, &value);
+        simple_cache.insert(&key2, &value);
+        simple_cache.get(&key1); // key2 is now the lru
+        simple_cache.insert(&key3, &value);
+        simple_cache.insert(&key4, &value);
 
         // Assert
-        assert_eq!(basic.lru_cache.len(), 3);
-        assert_eq!(basic.get(&key2), None); // lru is removed
-    }
-
-    #[fixture]
-    fn ttl() -> Cache<i32, i32> {
-        Cache::new(
-            3,
-            Duration::from_millis(200),
-        )
+        assert_eq!(simple_cache.lru_cache.len(), 3);
+        assert_eq!(simple_cache.get(&key2), None); // lru is removed
     }
 
     #[rstest]
-    fn ttl_expired(mut ttl: Cache<i32, i32>) {
+    fn ttl_expired(mut simple_cache: Cache<i32, i32>) {
         // Arrange
         let key = 1;
         let value = 2;
 
         // Act
-        ttl.insert(key, value);
+        simple_cache.insert(&key, &value);
         std::thread::sleep(std::time::Duration::from_millis(250));
 
         // Assert
-        assert_eq!(ttl.get(&key), None);
+        assert_eq!(simple_cache.get(&key), None);
+    }
+
+    #[rstest]
+    fn retrieve_or_compute_not_in_cache(mut simple_cache: Cache<i32, i32>){
+        // Arrange
+        let key = 1;
+
+        // Act
+        let (data, adhoc_code) = simple_cache.retrieve_or_compute(&key);
+
+        // Assert
+        assert_eq!(*data, 2);
+        assert_eq!(adhoc_code, 1);
+        assert_eq!(simple_cache.len(), 1);
+    }
+
+    #[rstest]
+    fn retrieve_or_compute_already_in_cache(mut simple_cache: Cache<i32, i32>){
+        // Arrange
+        let key = 1;
+
+        // Act
+        simple_cache.retrieve_or_compute(&key);
+        simple_cache.retrieve_or_compute(&key);
+        simple_cache.retrieve_or_compute(&key);
+        simple_cache.retrieve_or_compute(&key);
+        let (data, adhoc_code) = simple_cache.retrieve_or_compute(&key);
+
+        // Assert
+        assert_eq!(*data, 2);
+        assert_eq!(adhoc_code, 1);
+        assert_eq!(simple_cache.len(), 1);
+    }
+
+    #[rstest]
+    fn retrieve_or_compute_ttl_expired(mut simple_cache: Cache<i32, i32>){
+        // Arrange
+        let key = 1;
+
+        // Act
+        simple_cache.retrieve_or_compute(&key);
+        let expiration_1 = simple_cache.lru_cache.peek(&key).unwrap().expiration;
+        std::thread::sleep(std::time::Duration::from_millis(250));
+        simple_cache.retrieve_or_compute(&key);
+        let expiration_2 = simple_cache.lru_cache.peek(&key).unwrap().expiration;
+        
+
+        assert_ne!(expiration_1, expiration_2);      
     }
 
 }
